334e8b05884e4d54cfcabae5d87b266f159a22a3
Richie191 <richardmwilson191@gmail.com>
2019-12-16 23:25:38 -0500

    Working timer completed

---
+++ timer/timer.js
@@ -0,0 +1,46 @@
+class Timer {
+  constructor(durationInput, startButton, pauseButton, callBacks) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+
+    if (callBacks) {
+      this.onStart = callBacks.onStart;
+      this.onTick = callBacks.onTick;
+      this.onComplete = callBacks.onComplete;
+    }
+
+    this.startButton.addEventListener('click', this.start);
+    this.pauseButton.addEventListener('click', this.pause);
+  }
+
+  start = () => {
+    if (this.onStart) {
+      this.onStart(this.timeRemaining);
+    }
+    this.tick();
+    this.interval = setInterval(this.tick, 50);
+  };
+
+  tick = () => {
+    if (this.timeRemaining <= 0) {
+      this.pause();
+      this.onComplete();
+    } else {
+      this.timeRemaining = this.timeRemaining - 0.05;
+      this.onTick(this.timeRemaining);
+    }
+  };
+
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
+
+  set timeRemaining(time) {
+    this.durationInput.value = time.toFixed(2);
+  }
+
+  pause = () => {
+    clearInterval(this.interval);
+  };
+}
